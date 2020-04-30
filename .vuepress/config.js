9632506a6df6cb42abd5888cbeeb3759f083f2c1
Tiago Danin <TiagoDanin@outlook.com>
2020-04-30 16:44:51 -0300

    Usar Yuu em website

---
+++ .vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: 'Inspire OpenLung',
+	theme: 'yuu',
+  	themeConfig: {
+		yuu: {
+			defaultDarkTheme: false,
+			disableThemeIgnore: true,
+			defaultColorTheme: 'blue',
+		},
+		sidebar: "auto",
+		editLinks: true,
+		editLinkText: 'edita página',
+		nav: [
+			{ text: 'Início', link: '/' },
+			{ text: 'Apoiar esse Projeto', link: 'https://www.vakinha.com.br/vaquinha/inspire-respirador-da-escola-politecnica-da-usp-fase-2' },
+			{ text: 'Parceiros', link: '/extra/parceiros/' },
+			{ text: 'Contato', link: '/extra/contato' },
+		  ],
+	} 
+}
